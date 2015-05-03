ec848ce93990e8133944c5b89590ced0a615e6ba
yosefk <yosefk@spotoption.com>
2015-05-03 13:53:27 +0300

    version 1.0.0

--- README.md
+++ README.md
@@ -1,2 +1,63 @@
 # jQueryImageCaching
 jQuery plugin of image caching in localStorage
+
+**NOTICE:** The most of browsers memory limit of localStorage is 5MB.
+**NOTICE:** Correct way of image caching is using response of http server cache headers.
+
+### Version
+1.0.0
+
+## Requirements ##
+* jQuery [https://jquery.com/]
+
+## How to use ##
+Simple way
+```javascript
+<img data-src="path/to/image.(png,jpeg..)">
+
+<script>
+    $('img').templateLoader();
+</script>
+```
+
+May use renderCallback and attach on several images.
+```javascript
+<div class="user-img not-exist" data-src="path/to/image" data-caching-key="userImage"></div>
+
+<script>
+     $('.user-img').imageCaching({
+         crossOrigin:null, //need if using cookie or session for render of image
+         renderCallback:function(img,div){
+                    $(div).removeClass('not-exist').css('background-image', 'url(' + img + ')');
+         }});
+</script>
+```
+By default all images save with key nem his url, if you need custom key name - use attribute 'data-caching-key'.
+
+Fore refresh image better save of cachingObject.
+```javascript
+<img data-src="path/to/image">
+
+<script>
+  var cachingObject = $('img').templateLoader();
+  
+  $('button').on('click',function(){
+        cachingObject.refresh();
+  });
+</script>
+```
+If you will be delete image from localStorage.removeItem("path/to/image|or key") - new image will be loaded on refresh page.
+
+### More options
+> **debugMode** - default false, flag that give some console.log messages of working plugin for debug.
+
+> **sourceKeyAttribute** - default 'data-src', may change it on initialization
+
+> **cachingKeyAttribute** - default 'data-caching-key', may change it on initialization
+
+> **renderCallback** - default null, the callback after render image(change the selected item src). Arguments the image and html object
+where was changed attribute src.
+
+> **crossOrigin** - default 'Anonymous', origin of image in default mode not been send cookie or session id to server. Default
+mode may do problems for images that need authorization. This parameter change on problems with server headers. In most cases
+the set this params to "null" will be remove problems.
--- jquery.imageCaching.js
+++ jquery.imageCaching.js
@@ -1,3 +1,140 @@
 /**
- * Created by yosefk on 28/04/2015.
+ * jQuery ImageCaching plugin
+ * Caching selected images in localStorage
+ *
+ * Created by Yosef(Vlad) Kaminskyi
+ * Mailto: moledet[at]ukr.net
+ * Version:  1.0  on 03/05/2015.
+ * Dependencies:
+ *      jQuery https://jquery.com/
  */
+;(function($) {
+    function jQueryImageCaching(params){
+        var ImageCaching = {
+            selector: 'img',
+            debugMode: false,
+            cachingKeyAttribute:'data-caching-key',
+            sourceKeyAttribute:'data-src',
+            renderCallback:null,
+            crossOrigin: 'Anonymous',
+            init: function (params) {
+                ImageCaching.log('Initialization of ImageCaching');
+                for (var param in params) {
+                    ImageCaching[param] = params[param];
+                }
+
+                $(ImageCaching.selector).each(function(){
+                    ImageCaching.applyToImage($(this));
+                });
+            },
+            getCacheKey:function(element){
+                if(element.attr(ImageCaching.cachingKeyAttribute)){
+                    return  element.attr(ImageCaching.cachingKeyAttribute);
+                }else{
+                    return element.attr(ImageCaching.sourceKeyAttribute);
+                }
+            },
+            getCache:function(element){
+                var key = this.getCacheKey(element);
+                return localStorage.getItem(key);
+            },
+            setCache:function(element,imageData){
+                var key = ImageCaching.getCacheKey(element);
+                ImageCaching.log('Set cache',key,imageData,element);
+                localStorage.setItem(key, imageData); // save image data
+                return true;
+            },
+            removeCache:function(element){
+                var key = ImageCaching.getCacheKey(element);
+                ImageCaching.log('Remove cache',key);
+                localStorage.removeItem(key);
+                return true;
+            },
+            renderImage:function(element,picture){
+                ImageCaching.log('Rendering...',picture,element);
+                element.attr('src',picture);
+
+                if(this.renderCallback){
+                    ImageCaching.log('Render Callback...',element);
+                    this.renderCallback(picture,element);
+                }
+            },
+            applyToImage:function(element,force){
+                var cache = null;
+                if(!force){
+                    cache = this.getCache(element);
+                }
+
+                if(cache){
+                    ImageCaching.log('Image from cache',element);
+                    this.renderImage(element,cache);
+                }else{
+                    var sourceLink = element.attr(ImageCaching.sourceKeyAttribute);
+                    var getParamPrefix = "?";
+                    if(sourceLink.indexOf('?')>0){
+                        getParamPrefix = "&";
+                    }
+                    sourceLink+=getParamPrefix+'cacheTime='+Date.now();
+
+                    ImageCaching.log('Request to: '+sourceLink,element);
+
+                    var img = new Image();
+
+                    if(ImageCaching.crossOrigin){
+                        img.setAttribute('crossOrigin', 'Anonymous');
+                    }
+
+                    img.onload  = function() {
+                        ImageCaching.log('Loading completed',img);
+                        var canvas = document.createElement('canvas');
+                        canvas.width = img.width;
+                        canvas.height = img.height;
+
+                        // Copy the image contents to the canvas
+                        var ctx = canvas.getContext("2d");
+                        ctx.drawImage(img, 0, 0);
+
+                        document.body.appendChild(canvas);
+                        var context = canvas.getContext('2d');
+                        context.drawImage(img, 0, 0);
+
+                        var imageData = canvas.toDataURL("image/png");
+                        ImageCaching.setCache(element,imageData);
+                        ImageCaching.renderImage(element, imageData);
+                    }
+                    img.src = sourceLink;
+                }
+            },
+            refresh:function(itemsSelector){
+                var selector = null;
+                if(itemsSelector){
+                    selector = itemsSelector;
+                }else{
+                    selector = ImageCaching.selector;
+                }
+
+                $(selector).each(function(){
+                    ImageCaching.applyToImage($(this),true);
+                });
+
+            },
+            log: function () {
+                if (this.debugMode) {
+                    console.log(arguments);
+                }
+            }
+
+        };
+        ImageCaching.init(params);
+        return ImageCaching;
+    }
+
+    $.fn.extend({
+        imageCaching: function(options) {
+            var params = {selector:this};
+            params = $.extend(params,options);
+
+            return new jQueryImageCaching(params);
+        }
+    });
+})(jQuery);
